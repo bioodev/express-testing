63fff877e07cc2055507cca1c5f74dbf0c67ea47
enrique-propcheck.ai <bioodev@protonmail.com>
2024-10-03 17:44:09 -0300

    comentando conecccion mysql

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ const connection = mysql.createConnection({
 });
 
 // Conectar a la base de datos
-connection.connect((err) => {
-    console.log('Conectando a la base de datos MySQL...');
-    console.log('Host: ' + process.env.DB_HOST);
-    console.log('Puerto: ' + process.env.DB_PORT);
-    console.log('Usuario: ' + process.env.DB_USER);
-    console.log('Contraseña: ' + process.env.DB_PASSWORD);
-    console.log('Base de datos: ' + process.env.DB_DATABASE);  
-    if (err) {
-        console.error('Error conectando a la base de datos: ' + err.stack);
-        return;
-    }
-    console.log('Conectado a la base de datos MySQL como ID ' + connection.threadId);
-});
+// connection.connect((err) => {
+//     console.log('Conectando a la base de datos MySQL...');
+//     console.log('Host: ' + process.env.DB_HOST);
+//     console.log('Puerto: ' + process.env.DB_PORT);
+//     console.log('Usuario: ' + process.env.DB_USER);
+//     console.log('Contraseña: ' + process.env.DB_PASSWORD);
+//     console.log('Base de datos: ' + process.env.DB_DATABASE);  
+//     if (err) {
+//         console.error('Error conectando a la base de datos: ' + err.stack);
+//         return;
+//     }
+//     console.log('Conectado a la base de datos MySQL como ID ' + connection.threadId);
+// });
 
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo! Conectado a MySQL.');
