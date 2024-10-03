4d009c585a6297e63dbc66258bca573b2047a5c4
enrique-propcheck.ai <bioodev@protonmail.com>
2024-10-03 17:54:35 -0300

    comentando mas codigo

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ require('dotenv').config();
 
 
 // Configuración de la conexión a MySQL
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
 
 // Conectar a la base de datos
 // connection.connect((err) => {
